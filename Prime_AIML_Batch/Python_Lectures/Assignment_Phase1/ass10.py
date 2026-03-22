00b387034773013b14ee39e7257a64e2050a05c8
Atharv Navatre <navatreathrav@gmail.com>
2026-03-22 14:13:17 +0530

    Cleaned repo: removed notes and duplicate folders

--- Prime_AIML_Batch/Python_Lectures/Assignment_Phase1/ass10.py
+++
@@ -1,9 +0,0 @@
-# Program to separate integer and fractional parts
-
-num = float(input("Enter a decimal number: "))
-
-integer_part = int(num)
-fractional_part = num - integer_part
-
-print("Integer part:", integer_part)
-print("Fractional part:", fractional_part)
--- Prime_AIML_Batch/Python_Lectures/Assignment_Phase1/ass8.py
+++
@@ -1,5 +0,0 @@
-radius = float(input("Enter the radius: "))
-
-Area = 3.14 * radius * radius
-
-print("area of the circle: ", Area)
